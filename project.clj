c00fe54b9ab5e2ab8f8333a93f9f06efd95251d6
Peter Taoussanis <ptaoussanis@gmail.com>
2014-11-12 02:38:10 +0700

    v1.16.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.taoensso/encore "1.15.1"
+(defproject com.taoensso/encore "1.16.0"
   :author "Peter Taoussanis <https://www.taoensso.com>"
   :description "Shared support utils for taoensso.com Clojure libs"
   :url "https://github.com/ptaoussanis/encore"
