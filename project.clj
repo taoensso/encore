37a9f3f978ad666efaf1d713f5809af0e917346a
Peter Taoussanis <ptaoussanis@gmail.com>
2016-02-17 15:40:57 +0700

    v2.36.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.taoensso/encore "2.35.0"
+(defproject com.taoensso/encore "2.36.0"
   :author "Peter Taoussanis <https://www.taoensso.com>"
   :description "Core utils library for Clojure/Script"
   :url "https://github.com/ptaoussanis/encore"
@@ -15,7 +15,7 @@
   :dependencies
   [[org.clojure/clojure      "1.5.1"]
    [org.clojure/tools.reader "0.10.0"]
-   [com.taoensso/truss       "1.0.0"]]
+   [com.taoensso/truss       "1.1.0"]]
 
   :plugins
   [[lein-pprint  "1.1.2"]
