89e09ac5656369578cbd30ab0283c1bd3fe80c45
Peter Taoussanis <peter@taoensso.com>
2022-11-17 15:56:28 +0100

    v3.36.0 (2022-11-18)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.taoensso/encore "3.35.1"
+(defproject com.taoensso/encore "3.36.0"
   :author "Peter Taoussanis <https://www.taoensso.com>"
   :description "Core utils library for Clojure/Script"
   :url "https://github.com/ptaoussanis/encore"
