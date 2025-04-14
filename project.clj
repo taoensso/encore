abfeb45fe7c14f8750ccc65d0289e6259ae611dc
Peter Taoussanis <peter@taoensso.com>
2025-04-14 13:36:48 +0200

    [nop] Bump deps

--- project.clj
+++ project.clj
@@ -61,10 +61,10 @@
 
     :dependencies
     [[org.clojure/test.check "1.1.1"]
-     [org.clojure/core.async "1.7.701"]
-     [thheller/shadow-cljs   "2.28.22"]
+     [org.clojure/core.async "1.8.741"]
+     [thheller/shadow-cljs   "2.28.23"]
      [nrepl                  "1.3.1"]
-     [cider/cider-nrepl      "0.53.2"]
+     [cider/cider-nrepl      "0.55.1"]
      [com.google.javascript/closure-compiler-unshaded "v20240317"]]
 
     :plugins
